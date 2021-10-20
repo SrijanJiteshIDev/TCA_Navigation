a1221e58ca4296e5ced09381a36656dab76361c4
SrijanJiteshIDev <jitesh.acharya@srijan.net>
2021-10-20 12:54:15 +0530

    Initial Commit

---
+++ ExternalActionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExternalActionDemo
+//
+//  Created by Jitesh Acharya on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
